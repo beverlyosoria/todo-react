56b76d9f5b40b82baf27111eeea76e475e167fed
beverlyosoria <bevosoria@gmail.com>
2019-10-23 00:10:59 -0600

    updates class names for styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import 'App.css';
+import './App.css';
 
 class App extends Component {
 	constructor(props) {
@@ -46,28 +46,42 @@ class App extends Component {
 
 	render() {
 		return (
-			<div className="App">
-				<div>
-					Add and Item...
-					<br />
-					<input
-						type="text"
-						placeholder="Type item here..."
-						value={this.state.newItem}
-						onChange={(e) => this.updateInput('newItem', e.target.value)}
-					/>
-					<button onClick={() => this.addItem()}>Add</button>
-					<br />
-					<ul>
-						{this.state.list.map((item) => {
-							return (
-								<li key={item.id}>
-									{item.value}
-									<button onClick={() => this.deleteItem(item.id)}>X</button>
-								</li>
-							);
-						})}
-					</ul>
+			<div>
+				<h1 className="app-title">MY LIST</h1>
+				<div className="container">
+					<div
+						style={{
+							padding: 30,
+							textAlign: 'left',
+							maxWidth: 500,
+							margin: 'auto'
+						}}
+					>
+						Add and Item...
+						<br />
+						<input
+							type="text"
+							placeholder="Type item here..."
+							value={this.state.newItem}
+							onChange={(e) => this.updateInput('newItem', e.target.value)}
+						/>
+						<button className="add-btn btn-floating" onClick={() => this.addItem()}>
+							Add
+						</button>
+						<br />
+						<ul>
+							{this.state.list.map((item) => {
+								return (
+									<li key={item.id}>
+										{item.value}
+										<button className="btn btn-floating" onClick={() => this.deleteItem(item.id)}>
+											<i class="material-icons">x</i>
+										</button>
+									</li>
+								);
+							})}
+						</ul>
+					</div>
 				</div>
 			</div>
 		);
